7f3ef182fd64ff54cf97ca40a0df1921969cb814
arrem4eg <arem4eg@gmail.com>
2024-01-01 19:39:11 +0100

    added Book Store

---
+++ src/test/java/academy/mate/BookStore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.mate.BookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
