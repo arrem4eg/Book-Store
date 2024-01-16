6f1dc6f908c4c0997d738c80ad605ee64ba22459
arrem4eg <arem4eg@gmail.com>
2024-01-16 09:35:27 +0100

    fixed CreateBookRequestDto.java, deleted NotNull

--- src/main/java/academy/mate/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/academy/mate/bookstore/dto/CreateBookRequestDto.java
@@ -2,19 +2,15 @@ package academy.mate.bookstore.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
     @NotBlank
     private String title;
-    @NotNull
     @NotBlank
     private String author;
-    @NotNull
     @NotBlank
     private String isbn;
     @Min(0)
